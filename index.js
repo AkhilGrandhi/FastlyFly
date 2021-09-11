c7f41d122eda2e9b5729f810aee8cacb96ba4a9e
Grandhi Akhil Sai Manoj <55430217+AkhilGrandhi@users.noreply.github.com>
2021-09-11 15:58:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+$(".vision-btn").click(function(){
+    $('html,body').animate({
+        scrollTop: $(".ourvision").offset().top},'slow')
+});
+
+$(".mission-btn").click(function(){
+    $('html,body').animate({
+        scrollTop: $(".ourmission").offset().top},'slow')
+});
+
+$(".contactus-btn").click(function(){
+    $('html,body').animate({
+        scrollTop: $(".Contact-us").offset().top},'slow')
+});
+
+$(".teambtn").click(function(){
+    $('html,body').animate({
+        scrollTop: $(".team").offset().top},'slow')
+});
+
+$(".about-btn").click(function(){
+    $('html,body').animate({
+        scrollTop: $(".About-us").offset().top},'slow')
+});
+
+$(".enquiry-btn").click(function(){
+    $('html,body').animate({
+        scrollTop: $(".enquiry-form").offset().top},'slow')
+});
